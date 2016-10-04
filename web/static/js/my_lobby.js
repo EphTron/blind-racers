899e90425ff864189fe1ca3143f6c45caaa7455a
EphTron <es.90@gmx.de>
2016-10-04 23:18:55 +0200

    added static folder with font awesome, and css

--- web/static/js/my_lobby.js
+++ web/static/js/my_lobby.js
@@ -11,17 +11,23 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations
 // under the License.
+var players = "";
+
+function set_players(val)
+{
+    players = val;
+    test = "{ 'players' :"+val +"}";
+    console.log("Players: ",players, test);
+
+    check = JSON.parse(test);
+    console.log("") 
+}
 
 $(document).ready(function() {
     if (!window.console) window.console = {};
     if (!window.console.log) window.console.log = function() {};
 
-    $("#messageform").on("submit", function() {
-        newMessage($(this));
-        return false;
-    });
-
-    $("#lobby-table").on("submit", function() {
+    $("#stateform").on("submit", function() {
         console.log("Hello")
         state_changer = document.querySelector("#hidden-state-field")
         if (state_changer.value == 1) {
@@ -29,31 +35,15 @@ $(document).ready(function() {
         } else if (state_changer.value == 0) {
             state_changer.value = 1;
         }
-        //button = document.querySelector("#ready-button")
-        
-        /*if (button.value == 1) {
-            button.value = 0;
-            button.innerHTML = "Ready";
-            button.class = "lobby-ready-button not-ready";
-        } else if (button.value == 0) {
-            button.value = 1;
-            button.innerHTML = "Wait"
-            button.class = "lobby-ready-button ready";
-        }*/
-        sendState($(this));
+        newMessage($(this));
         return false;
     });
+    //console.log("Getting players")
+    //var players = JSON.parse(json);
+    //console.log("Got players", players)
 
-    $("#messageform").on("keypress", function(e) {
-        if (e.keyCode == 13) {
-            newMessage($(this));
-            return false;
-        }
-        return true;
-    });
-    $("#message").select();
+    $("#hidden-state-field").select();
     updater.poll();
-    lobbyUpdater.poll();
 });
 
 function newMessage(form) {
@@ -61,7 +51,8 @@ function newMessage(form) {
     console.log("message", message)
     var disabled = form.find("input[type=submit]");
     disabled.disable();
-    $.postJSON("/chat/new", message, function(response) {
+    var address = window.location.pathname+"/change";
+    $.postJSON(address, message, function(response) {
         updater.showMessage(response);
         if (message.id) {
             form.parent().remove();
@@ -72,7 +63,7 @@ function newMessage(form) {
     });
 }
 
-function sendState(form) {
+/*function sendState(form) {
     console.log("send State")
     var state = form.formToDict();
     console.log("State:",state);
@@ -88,7 +79,7 @@ function sendState(form) {
             disabled.enable();
         }
     });
-}
+}*/
 
 function changeValue() {
     // Changes the value of the button
@@ -149,20 +140,28 @@ jQuery.fn.enable = function(opt_enable) {
 };
 
 var updater = {
+
     errorSleepTime: 500,
     cursor: null,
+    address: window.location.pathname+"/update",
     poll: function() {
         var args = {"_xsrf": getCookie("_xsrf")};
         if (updater.cursor) args.cursor = updater.cursor;
-        $.ajax({url: "/chat/update", type: "POST", dataType: "text",
-                data: $.param(args), success: updater.onSuccess,
+        $.ajax({url: updater.address, 
+                type: "POST", 
+                dataType: "text",
+                data: $.param(args), 
+                success: updater.onSuccess,
                 error: updater.onError});
     },
 
     onSuccess: function(response) {
         try {
-            console.log(response)
-            updater.newMessages(eval("(" + response + ")"));
+            console.log("wooooorks",response)
+            //var json = '{"result":true,"count":1}',
+            var player = JSON.parse(response);
+            updater.newMessages(player);
+            //updater.newMessages(eval("(" + response + ")"));
         } catch (e) {
             updater.onError();
             return;
@@ -173,21 +172,23 @@ var updater = {
 
     onError: function(response) {
         updater.errorSleepTime *= 2;
-        console.log("Poll error; sleeping for", updater.errorSleepTime, "ms");
+        console.log("Poll error; sleeping for", response,updater.errorSleepTime, "ms");
         window.setTimeout(updater.poll, updater.errorSleepTime);
     },
 
     newMessages: function(response) {
-        if (!response.messages) return;
-        updater.cursor = response.cursor;
+        console.log("Received",response)
+        if (!response.id) return;
+        console.log("yep")
+        updater.cursor = response.player.id;
         console.log("upd.cursor", updater.cursor)
-        var messages = response.messages;
-        console.log("messsge", messages)
-        updater.cursor = messages[messages.length - 1].id;
-        console.log(messages.length, "new messages, cursor:", updater.cursor);
-        for (var i = 0; i < messages.length; i++) {
-            updater.showMessage(messages[i]);
-        }
+        
+
+        //updater.cursor = newMessagesssages[messages.length - 1].id;
+        //console.log(messages.length, "new messages, cursor:", updater.cursor);
+        //for (var i = 0; i < messages.length; i++) {
+        //    updater.showMessage(messages[i]);
+        //}
     },
 
     showMessage: function(message) {
@@ -199,64 +200,3 @@ var updater = {
         node.slideDown();
     }
 };
-
-var lobbyUpdater = {
-    errorSleepTime: 500,
-    cursor: null,
-    address: window.location.pathname+"/update",
-    poll: function() {
-        console.log("poll")
-        var args = {"_xsrf": getCookie("_xsrf")};
-        if (lobbyUpdater.cursor) args.cursor = lobbyUpdater.cursor;
-        $.ajax({url: lobbyUpdater.address, 
-                type: "POST",
-                dataType: "text",
-                data: $.param(args), 
-                success: lobbyUpdater.onSuccess,
-                error: lobbyUpdater.onError});
-    },
-
-    onSuccess: function(response) {
-        console.log("succ")
-        try {
-            console.log(response)
-            lobbyUpdater.newMessages(eval("(" + response + ")"));
-        } catch (e) {
-            lobbyUpdater.onError();
-            return;
-        }
-        lobbyUpdater.errorSleepTime = 500;
-        window.setTimeout(lobbyUpdater.poll, 0);
-    },
-
-    onError: function(response) {
-        console.log("err")
-        lobbyUpdater.errorSleepTime *= 2;
-        console.log("Poll error; sleeping for",lobbyUpdater.address, lobbyUpdater.errorSleepTime, "ms");
-        window.setTimeout(lobbyUpdater.poll, lobbyUpdater.errorSleepTime);
-    },
-
-    newMessages: function(response) {
-        console.log("new")
-        if (!response.messages) return;
-        lobbyUpdater.cursor = response.cursor;
-        var messages = response.messages;
-        console.log("messsge", messages)
-        lobbyUpdater.cursor = messages[messages.length - 1].id;
-        console.log(messages.length, "new messages, cursor:", lobbyUpdater.cursor);
-        for (var i = 0; i < messages.length; i++) {
-            lobbyUpdater.showMessage(messages[i]);
-        }
-    },
-
-    showMessage: function(message) {
-        console.log("show")
-        var existing = $("#m" + message.id);
-        if (existing.length > 0) return;
-        var node = $(message.html);
-        node.hide();
-        $("#inbox").append(node);
-        node.slideDown();
-    }
-};
-
